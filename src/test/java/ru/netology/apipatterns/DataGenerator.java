fef331296bfa5fe07d22be7ea15f8a0881cf0ef0
MariaG <MGrigorenko@bellintegrator.com>
2022-11-30 14:21:15 +0300

    modified README.md

---
+++ src/test/java/ru/netology/apipatterns/DataGenerator.java
@@ -0,0 +1,65 @@
+package ru.netology.apipatterns;
+
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.Value;
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.*;
+
+public class DataGenerator {
+    private static final RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    private DataGenerator() {
+    }
+
+    private static void sendRequest(RegistrationDto registeredUser) {
+        given().
+                spec(requestSpec).
+                body(registeredUser).
+                when().
+                post("/api/system/users").
+                then().
+                statusCode(200);
+    }
+
+    public static String getRandomLogin() {
+        String login = faker.name().username();
+        return login;
+    }
+
+    public static String getRandomPassword() {
+        String password = faker.internet().password();
+        return password;
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static RegistrationDto getRegisteredUser(String status) {
+            RegistrationDto notRegisteredUser = new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
+            RegistrationDto registeredUser = new RegistrationDto(getRandomLogin(), getRandomPassword(), status);
+            sendRequest(registeredUser);
+            return registeredUser;
+        }
+    }
+
+    @Value
+    public static class RegistrationDto {
+        String login;
+        String password;
+        String status;
+    }
+}
